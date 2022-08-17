48db784d4598b041d70a758d42f33211b294809f
tariktesfa <tariktesfa9090@gmail.com>
2022-08-17 16:12:44 +0300

    Exercises on more Data Structures in Python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    return list(map(lambda newMatrix: list(map(lambda v: v*v,
+                newMatrix)), matrix))
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+## Python - More Data Structures: Set, Dictionary
+
+
